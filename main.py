7027d147d4c411105ad0ddeab6d2be577ea293d5
Igor Leonenko <igrleon@gmail.com>
2024-10-27 21:30:57 +0100

    disable args

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ class Checker:
     def __init__(self):
         if os.getenv("GITHUB_ACTIONS") == "true":
             chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--no-sandbox")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--disable-gpu")
+            # chrome_options.add_argument("--headless")
+            # chrome_options.add_argument("--no-sandbox")
+            # chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--remote-debugging-port=9222")
             self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         else:
