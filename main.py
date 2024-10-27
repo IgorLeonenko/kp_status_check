7c2c02740ffb1c05dd18a40661ebca5c5c21e486
Igor Leonenko <igrleon@gmail.com>
2024-10-27 20:29:50 +0100

    update options

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ class Checker:
     def __init__(self):
         if os.getenv("GITHUB_ACTIONS") == "true":
             chrome_options = Options()
+            chrome_options.add_argument("--headless")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--window-size=1920,1080")
+            chrome_options.add_argument("--remote-debugging-port=9222")
             self.driver = webdriver.Chrome(options=chrome_options)
         else:
             self.driver = webdriver.Chrome(service=Service("/Users/leon/projects/selenium/chromedriver"))
@@ -23,6 +25,7 @@ class Checker:
 
     def process(self):
         try:
+            sleep(15)
             self.driver.get("https://pio-przybysz.duw.pl/login")
             sleep(5)
             self.driver.find_element(By.ID, "mat-input-0").send_keys(os.getenv('EMAIL_TO'))
