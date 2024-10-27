cd02818a97a4a77e858aaa59bafaf44770661e3d
Igor Leonenko <igrleon@gmail.com>
2024-10-27 21:21:42 +0100

    remove option

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ class Checker:
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--window-size=1920,1080")
             chrome_options.add_argument("--remote-debugging-port=9222")
             self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         else:
