9af490a798c8327d90f716ebe5596481bebbbdbe
Igor Leonenko <igrleon@gmail.com>
2024-10-27 20:20:51 +0100

    remove headless

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class Checker:
     def __init__(self):
         if os.getenv("GITHUB_ACTIONS") == "true":
             chrome_options = Options()
-            chrome_options.add_argument("--headless")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--disable-gpu")
